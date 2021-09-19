289a35425a7abfe13f4e1e329925202644b21420
PropeReferio <lemuel.b.stevens@gmail.com>
2021-09-19 17:35:43 -0500

    style reqs

--- find_ids_by_symbol.py
+++ find_ids_by_symbol.py
@@ -1,16 +1,15 @@
-from requests import Session
 import json
 import os
 
+from requests import Session
+
 url = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map"
 
-parameters = {
-        'symbol': 'MATIC,XLM,COMP,BAL,CLV,GRT,ANKR,BOND,SKL,NU,FORTH'
-}
+parameters = {"symbol": "MATIC,XLM,COMP,BAL,CLV,GRT,ANKR,BOND,SKL,NU,FORTH"}
 
 headers = {
-    'Accepts': 'application/json',
-    'X-CMC_PRO_API_KEY': os.environ['CMC_PRO_API_KEY'],
+    "Accepts": "application/json",
+    "X-CMC_PRO_API_KEY": os.environ["CMC_PRO_API_KEY"],
 }
 
 session = Session()
@@ -19,7 +18,7 @@ session.headers.update(headers)
 current_prices = []
 
 response = session.get(url, params=parameters)
-data = json.loads(response.text)['data']
+data = json.loads(response.text)["data"]
 
 for coin in data:
     print(f"Coin: {coin['name']}, Symbol: {coin['symbol']}, ID: {coin['id']}")
